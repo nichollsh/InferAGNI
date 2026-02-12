c6c7652eb73513281335d1cf3c2ad1fa340ab8d8
Harrison Nicholls <harrison.nicholls@ast.cam.ac.uk>
2026-02-12 13:59:23 +0000

    Linear interpolation from grid in N-dimensions. Tidied up utils

--- src/inferagni/grid.py
+++ src/inferagni/grid.py
@@ -9,9 +9,28 @@ from .const import *
 
 class Grid:
     def __init__(self, data_dir):
+
+        # scalar data
         self.data, self.input_keys, self.output_keys = self._load_from_dir(data_dir)
 
+        # profiles data
+        self.profs = None # TODO
+
+        # emission data
+        self.emits = None # TODO
+
+        # interpolator
+        self.interp = None # Instantiated later
+
+
     def _load_from_dir(self, data_dir):
+        """Load grid data from CSV files in the specified directory.
+
+        Parameters
+        -----------
+        - data_dir : str The directory containing the CSV files.
+
+        """
 
         print("Loading data from CSV files...")
         print(f"Data directory: {data_dir}")
@@ -20,7 +39,7 @@ class Grid:
         gridpoints_df = pd.read_csv(os.path.join(data_dir, "nogit_points.csv"), sep=',')
 
         # Read the consolidated results file
-        results_df = pd.read_csv(os.path.join(data_dir, "nogit_results.csv"), sep=',')
+        results_df = pd.read_csv(os.path.join(data_dir, "nogit_table.csv"), sep=',')
 
         # Merge the dataframes on index
         data = pd.merge(gridpoints_df, results_df, on="index")
@@ -45,15 +64,32 @@ class Grid:
         return data, input_keys, output_keys
 
     def show_inputs(self):
+        """Print the unique values of each input variable in the grid."""
         unique = {}
         for key in self.input_keys:
             unique[key] = np.unique(self.data[key].values)
             print(f"{key:12s}\n\t- {unique[key]}")
         return unique
 
-    def griddata_2d(self,
+    def interpolate_2d(self,
                     zkey=None,controls=None,
                     resolution=100, method='linear'):
+        """Interpolate a 2D grid of z-values as a function of mass and radius.
+
+        Parameters
+        -----------
+        - zkey : str The name of the variable to interpolate
+        - controls : dict A dictionary of control variables to filter the data by.
+        - resolution : int The number of points to interpolate along each axis (mass and radius).
+        - method : str The interpolation method to use. Options are 'linear', 'nearest', and 'cubic'.
+
+        Returns
+        -----------
+        - itp_x : 2D array The x-coordinates of the interpolated grid (mass).
+        - itp_y : 2D array The y-coordinates of the interpolated grid (radius).
+        - itp_z : 2D array The interpolated z-values on the grid (the variable specified by zkey).
+        """
+
 
         from scipy.interpolate import griddata
 
@@ -86,8 +122,10 @@ class Grid:
         print(f"Number of points: {len(subdata)}")
 
         # Flatten data
-        points = np.array([subdata["mass_tot"].values, subdata["r_phot"].values/R_earth]).T
-        values = np.array(subdata[zkey].values)
+        points = np.array([subdata["mass_tot"].values*units["r_phot"][0],
+                           subdata["r_phot"].values*units["r_phot"][0]
+                           ]).T
+        values = np.array(subdata[zkey].values*units[zkey][0])
 
         # Get range on x,y keys
         x_min, x_max = np.min(points[:,0]), np.max(points[:,0])
@@ -96,14 +134,33 @@ class Grid:
         print(f"y range: {y_min:.2f} - {y_max:.2f}") # radius
 
         # Target grid to interpolate to
-        grid_x, grid_y = np.meshgrid(np.linspace(x_min, x_max, resolution),
+        itp_x, itp_y = np.meshgrid(np.linspace(x_min, x_max, resolution),
                                      np.linspace(y_min, y_max, resolution),
                                      indexing='ij'
                                      )
 
 
         # Do the interpolation
-        grid_z = griddata(points, values, (grid_x, grid_y), method=method)
+        itp_z = griddata(points, values, (itp_x, itp_y), method=method)
 
         # Return the grid for plotting
-        return grid_x, grid_y, grid_z
+        return itp_x, itp_y, itp_z
+
+
+    def interp_init(self, vkey:str="r_phot"):
+        """Instantiate an interpolator on the grid data.
+
+        Parameters
+        -----------
+        - vkey : str The name of the variable to interpolate (e.g. "r_phot").
+        """
+
+        from scipy.interpolate import LinearNDInterpolator
+
+        # gather data
+        xyz = np.array([self.data[k].values for k in self.input_keys[2:]]).T
+        v = np.array(self.data[vkey].values)
+
+        # instantiate interpolator
+        self.interp = LinearNDInterpolator(xyz, v)
+
