684e0de25bf25606f255597705a4a0ae8ad19bc5
Harrison Nicholls <harrison.nicholls@ast.cam.ac.uk>
2026-02-12 13:59:23 +0000

    Linear interpolation from grid in N-dimensions. Tidied up utils

--- src/inferagni/__init__.py
+++ src/inferagni/__init__.py
@@ -4,6 +4,7 @@ __version__ = '26.02.12'
 
 import inferagni.grid
 import inferagni.cli
+import inferagni.const
 import inferagni.plot
 
-__all__ = ["grid", "cli", "plot"]
+__all__ = ["grid", "cli", "const", "plot"]
